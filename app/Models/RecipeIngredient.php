0847bd0a36fffe894e25f6653aba838d10ef26c5
Hlucht <luchtembergh@gmail.com>
2023-02-22 23:51:19 +0000

    Função que retorna os ingredientes de uma receita

--- app/Models/RecipeIngredient.php
+++ app/Models/RecipeIngredient.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class RecipeIngredient extends Model
 {
@@ -19,12 +20,30 @@ class RecipeIngredient extends Model
     ];
 
 
-    public function ingredient(){
+    public function ingredient()
+    {
         return $this->belongsTo(Ingredient::class);
     }
 
-    
-    public function recipe(){
+
+    public function recipe()
+    {
         return $this->belongsTo(Recipe::class);
+    }
+
+
+    public function ingredients_recipe($id_recipe)
+    {
+        $ingredient_ids = DB::table('recipe_ingredients')->where('recipe_id', $id_recipe)->pluck('ingredient_id')->all();
+
+        $ingredients = [];
+        foreach ($ingredient_ids as  $ingredient_id) {
+            $ingredients = DB::table('ingredients')->where('id', $ingredient_ids)->get();
+        }
+
+        return  $ingredients;
+
+        //Funcionalidade não finalizada - Os ingredientes não foram recuperados, a variável está sendo 
+        //sobreescrita e por isso o retorno tráz apenas o último valor.
     }
 }
