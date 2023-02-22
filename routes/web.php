c7ac16c8c02d43d9a0b5b8680ec3fb4fffcb20ab
Hlucht <luchtembergh@gmail.com>
2023-02-22 23:38:59 +0000

    Adicionado rota de edição e exclusão de receita

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,11 @@ Route::post('ingrediente/edicao/{id}', 'App\Http\Controllers\IngredientControlle
 //Página de receitas
 Route::resource('/receitas', 'App\Http\Controllers\RecipeController');
 
+//Exclui uma receita
+Route::post('/receita/{id}', 'App\Http\Controllers\RecipeController@destroy');
+
 //Página de receita com os respectivos ingredientes
 Route::resource('/receita', 'App\Http\Controllers\RecipeIngredientController');
+
+//Edita uma receita
+Route::post('receita/edicao/{id}', 'App\Http\Controllers\RecipeController@update');
