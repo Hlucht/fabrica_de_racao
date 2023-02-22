221d62021f971421381989ccfd8ae88690db6cab
Hlucht <luchtembergh@gmail.com>
2023-02-22 23:38:22 +0000

    Adicionando rotas de edição e exclusão de ingrediente

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 //Página de ingredientes
 Route::resource('/ingredientes', 'App\Http\Controllers\IngredientController');
 
+//Exclui um ingrediente
+Route::post('/ingrediente/{id}', 'App\Http\Controllers\IngredientController@destroy');
+
+//Edita um ingrediente
+Route::post('ingrediente/edicao/{id}', 'App\Http\Controllers\IngredientController@update');
+
+
+
 //Página de receitas
 Route::resource('/receitas', 'App\Http\Controllers\RecipeController');
 
